81199c2b2b5585bf0c643fd40433a30e7d1b527b
Suraj <1507surajkuamr@gmail.com>
2022-06-29 05:44:52 -0400

    commits

--- B.java
+++ B.java
@@ -4,6 +4,7 @@ public class B {
     static int j=20;
     public static void main(String[] args) {
         B b1=new B();
+
         System.out.println(b1.i);
         System.out.println(B.j);
     }
