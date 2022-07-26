82d285c36af9431c4390fc127d76c92487fc0654
Suraj <1507surajkuamr@gmail.com>
2022-07-26 22:04:52 +0530

    commits

---
+++ Day_2_July_22/A.java
@@ -0,0 +1,11 @@
+package Day_2_July_22;
+
+public class A {
+    int i = 10;
+    static int j = 500;
+    public static void main(String[] args){
+        A a1 = new A();
+        System.out.println(a1.i);
+        System.out.println(A.j);
+    }
+}
---
+++ Day_2_July_22/B.java
@@ -0,0 +1,11 @@
+package Day_2_July_22;
+
+public class B {
+        int l = 10;
+        static int j = 500;
+        public static void main(String[] args){
+            B a1 = new B();
+            System.out.println(a1.l);
+            System.out.println(B.j);
+        }
+    }
