c755da88eec551903ce6c41589499930a752aa3f
Suraj <1507surajkuamr@gmail.com>
2022-07-30 22:17:48 +0530

    commits

---
+++ NEW_KEYWORDS/A.java
@@ -0,0 +1,14 @@
+package NEW_KEYWORDS;
+/*
+-> It sends request to the class so that it can create objects
+-> Once object is created new keyword get the object address and stores
+in reference variable
+ */
+public class A {
+    public static void main (String[] args) {
+        A a1 = new A();
+        System.out.println(a1);
+        A a2 = new A();
+        System.out.println(a2);
+    }
+}
