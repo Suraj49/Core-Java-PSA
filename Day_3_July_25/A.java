e0e7f3a946ef510a91429a235c72d02b55fe6b24
Suraj <1507surajkuamr@gmail.com>
2022-08-02 13:06:58 +0530

    commits

--- Day_3_July_25/A.java
+++ Day_3_July_25/A.java
@@ -1,4 +1,17 @@
 package Day_3_July_25;
-
+//methods
 public class A {
-}
+        public static void main(String[] args) {
+            METHODS.A.test1();//accessing static method using class name
+            test1();//we can also access like that
+            METHODS.A a1=new METHODS.A();
+            a1.test2();//accessing non static method using object
+        }
+        public static void test1(){
+            System.out.println("static method");
+        }
+        public void test2(){
+            System.out.println("non static method");
+        }
+
+    }
