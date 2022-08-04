22b77ef8f18fe9460282e29a9d0995de8739c66a
Suraj <1507surajkuamr@gmail.com>
2022-08-04 22:09:25 +0530

    commits

--- Day_7_Aug_29/B.java
+++ Day_7_Aug_29/B.java
@@ -1,8 +1,12 @@
 package Day_7_Aug_29;
 //ref variable 2.static ref
 public class B {
-    static B b1=new B(); //static ref. we can access static
+    static B b1=new B(); //static ref. we can access static ref variable anywhere in same class
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println(b1); //we can use static ref variable in any method
+        b1.test();
+    }
+    public void test(){
+        System.out.println(b1);
     }
 }
