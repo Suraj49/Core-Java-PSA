4dfda3fd5c059a7306d93b193f6fa917387c051b
Suraj <1507surajkuamr@gmail.com>
2022-08-06 00:12:12 +0530

    commits

--- Day_10_Aug_05/B.java
+++ Day_10_Aug_05/B.java
@@ -1,7 +1,16 @@
 package Day_10_Aug_05;
 //calling constructor using this();
 public class B {
+    B(){
+        this(10); // we can call constructor using this();
+        //while calling constructor using this() ensure that this calling being done form
+        //another constructor .
+        //while calling const. using this() make sure that this shuld be the very first statement in onther const.
+    }
+    B(int i){
+        System.out.println(i);
+    }
     public static void main(String[] args) {
-        B b1=new B();
+        B b1=new B();//object is created constructor is called
     }
 }
