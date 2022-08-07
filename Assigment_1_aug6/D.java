c8a1d824613aebd749bdaf75440ed4326cde578a
Suraj <1507surajkuamr@gmail.com>
2022-08-07 12:52:41 +0530

    commits

---
+++ Assigment_1_aug6/D.java
@@ -0,0 +1,11 @@
+package Assigment_1_aug6;
+
+public class D {
+    public static void main(String[] args) {
+        //pre decrement
+        int o=24;
+        int p=--o; // pre decrement --o decrement the value 1 at same level
+        System.out.println(o); // here p=--o ; p=--24 decrement at same level so p=23;
+        System.out.println(p); // and also --o it means --24 is o=23;
+    }
+}
