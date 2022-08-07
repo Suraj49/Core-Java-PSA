4b8e592aa1a7dffb65342467870d9ea05e935dea
Suraj <1507surajkuamr@gmail.com>
2022-08-07 12:51:52 +0530

    commits

---
+++ Assigment_1_aug6/C.java
@@ -0,0 +1,12 @@
+package Assigment_1_aug6;
+
+public class C {
+    public static void main(String[] args) {
+        //decrement
+        //post decrement
+        int l = 16;
+        int m = l--; //post decrement l-- when we see next l then decresed the value by one
+        System.out.println(l); // value decresed by 1: 16-1=15
+        System.out.println(m); // but value of m=l-- it means m=16-- so j=16;
+    }
+}
