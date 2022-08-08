453c9fb01eb471154e53d87af5b870c465835fff
Suraj <1507surajkuamr@gmail.com>
2022-08-08 16:37:31 +0530

    commits

---
+++ Day_12_Aug_08/INHERITANCE/A.java
@@ -0,0 +1,6 @@
+package Day_12_Aug_08.INHERITANCE;
+//we inherit the properties of parent class to child class with intension to reusing
+//it.
+public class A {
+    int x=10; //non static variable
+}
