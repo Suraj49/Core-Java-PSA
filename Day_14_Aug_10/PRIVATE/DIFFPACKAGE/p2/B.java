3585a84d374cbe6860bcd03d40a8c7c06e7fa6ae
Suraj <1507surajkuamr@gmail.com>
2022-08-10 21:29:33 +0530

    commits

--- Day_14_Aug_10/PRIVATE/DIFFPACKAGE/p2/B.java
+++ Day_14_Aug_10/PRIVATE/DIFFPACKAGE/p2/B.java
@@ -1,4 +1,13 @@
 package Day_14_Aug_10.PRIVATE.DIFFPACKAGE.p2;
 
-public class B {
+import Day_14_Aug_10.PRIVATE.DIFFPACKAGE.p1.A;
+
+// different package sub class using inheritance we cannot access
+public class B extends A {
+    public static void main(String[] args) {
+        B b1=new B();
+       //  System.out.println(b1.x); Error
+       // b1.test(); Error
+    }
+
 }
