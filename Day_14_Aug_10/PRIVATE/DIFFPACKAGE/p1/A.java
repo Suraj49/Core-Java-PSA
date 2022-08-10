e828ce2e34408ee7c7ab146bdc765f49832f6a94
Suraj <1507surajkuamr@gmail.com>
2022-08-10 21:25:57 +0530

    commits

---
+++ Day_14_Aug_10/PRIVATE/DIFFPACKAGE/p1/A.java
@@ -0,0 +1,8 @@
+package Day_14_Aug_10.PRIVATE.DIFFPACKAGE.p1;
+
+public class A {
+    private int i=20; //private variable
+    private void test(){ //private method
+        System.out.println(200);
+    }
+}
---
+++ Day_14_Aug_10/PRIVATE/DIFFPACKAGE/p2/B.java
@@ -0,0 +1,4 @@
+package Day_14_Aug_10.PRIVATE.DIFFPACKAGE.p2;
+
+public class B {
+}
