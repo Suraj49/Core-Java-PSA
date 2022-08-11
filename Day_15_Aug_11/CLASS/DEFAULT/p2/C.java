446004ec0d57407b66f9ad887a2f0d239a3e5a1b
Suraj <1507surajkuamr@gmail.com>
2022-08-11 21:38:27 +0530

    commits

---
+++ Day_15_Aug_11/CLASS/DEFAULT/p2/C.java
@@ -0,0 +1,10 @@
+package Day_15_Aug_11.CLASS.DEFAULT.p2;
+
+//import Day_15_Aug_11.CLASS.DEFAULT.p1.A; error make A public
+
+public class C {
+    public static void main(String[] args) {
+       // A a1=new A();
+
+    }
+}
