3f42e6c94cb4971a22b2f72d41ef421c4fc9e8ec
Suraj <1507surajkuamr@gmail.com>
2022-08-11 21:34:27 +0530

    commits

---
+++ Day_15_Aug_11/CLASS/DEFAULT/p1/A.java
@@ -0,0 +1,9 @@
+package Day_15_Aug_11.CLASS.DEFAULT.p1;
+// A class can not be private.
+//A class can not be protected.
+ class A { //default class : accessed into same package not in different package
+     public int i=10;
+     public void test(){
+         System.out.println("default class package p1");
+     }
+}
---
+++ Day_15_Aug_11/CLASS/DEFAULT/p1/B.java
@@ -0,0 +1,9 @@
+package Day_15_Aug_11.CLASS.DEFAULT.p1;
+
+ class B {
+  public static void main(String[] args) {
+   A a1=new A();
+   System.out.println(a1.i);
+   a1.test();
+  }
+}
