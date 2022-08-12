25bf91b1ac788ab4c1d77bd0568e64cba23424d3
Suraj <1507surajkuamr@gmail.com>
2022-08-12 20:14:10 +0530

    commits

---
+++ Day_15_Aug_11/CONSTRUCTOR/PRIVATE/p2/C.java
@@ -0,0 +1,9 @@
+package Day_15_Aug_11.CONSTRUCTOR.PRIVATE.p2;
+
+import Day_15_Aug_11.CONSTRUCTOR.PRIVATE.p1.A;
+
+public class C {
+    public static void main(String[] args) {
+//        A a=new A(); we can't create when constructor is private
+    }
+}
