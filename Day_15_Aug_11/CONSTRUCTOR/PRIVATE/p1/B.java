abce66f56756be15c22c563611f5be7730654ae6
Suraj <1507surajkuamr@gmail.com>
2022-08-12 20:06:37 +0530

    commits

---
+++ Day_15_Aug_11/CONSTRUCTOR/PRIVATE/p1/B.java
@@ -0,0 +1,7 @@
+package Day_15_Aug_11.CONSTRUCTOR.PRIVATE.p1;
+
+public class B {
+    public static void main(String[] args) {
+        //   A a=new A(); if constructor is private then we can not create object
+    }
+}
