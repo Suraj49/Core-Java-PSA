4c8563b1cc3fe50ac66d7205548a5e644028cd77
Suraj <1507surajkuamr@gmail.com>
2022-08-13 21:58:05 +0530

    commits

---
+++ Day_16_Aug_12/POLYMORPHISM/OVERLOADING/C.java
@@ -0,0 +1,8 @@
+package Day_16_Aug_12.POLYMORPHISM.OVERLOADING;
+
+public class C {
+    static int i=10;
+    static void test(){
+        System.out.println("static");
+    }
+}
---
+++ Day_16_Aug_12/POLYMORPHISM/OVERLOADING/D.java
@@ -0,0 +1,8 @@
+package Day_16_Aug_12.POLYMORPHISM.OVERLOADING;
+
+public class D extends C{
+    public static void main(String[] args) {
+        System.out.println(D.i);
+        D.test();
+    }
+}
