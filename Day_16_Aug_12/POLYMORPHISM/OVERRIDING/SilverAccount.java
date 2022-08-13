4a0cc31c7feac949aae6a589534a04b7032b6762
Suraj <1507surajkuamr@gmail.com>
2022-08-13 21:29:21 +0530

    commits

---
+++ Day_16_Aug_12/POLYMORPHISM/OVERRIDING/SilverAccount.java
@@ -0,0 +1,16 @@
+package Day_16_Aug_12.POLYMORPHISM.OVERRIDING;
+
+public class SilverAccount {
+    public void chqBook(){
+        System.out.println("2 Chq Books");
+    }
+    public void phBanking(){
+        System.out.println("Yes");
+    }
+    public void atmCard(){
+        System.out.println("Yes");
+    }
+    public void isLoan(){
+        System.out.println("No");
+    }
+}
