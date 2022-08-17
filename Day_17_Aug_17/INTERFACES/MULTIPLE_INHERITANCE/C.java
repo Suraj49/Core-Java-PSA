12a0233314c4b8c08e4c14d92bd12caae19a76d5
Suraj <1507surajkuamr@gmail.com>
2022-08-17 12:45:13 +0530

    commits

---
+++ Day_17_Aug_17/INTERFACES/MULTIPLE_INHERITANCE/C.java
@@ -0,0 +1,5 @@
+package Day_17_Aug_17.INTERFACES.MULTIPLE_INHERITANCE;
+
+public interface C extends A,B{
+    public void test3();
+}
---
+++ Day_17_Aug_17/INTERFACES/MULTIPLE_INHERITANCE/D.java
@@ -0,0 +1,27 @@
+package Day_17_Aug_17.INTERFACES.MULTIPLE_INHERITANCE;
+
+public class D implements C{
+    @Override
+    public void test1() {
+        System.out.println(1);
+    }
+
+    @Override
+    public void test2() {
+
+        System.out.println(2);
+    }
+
+    @Override
+    public void test3() {
+
+        System.out.println(3);
+    }
+
+    public static void main(String[] args) {
+        D d1=new D();
+        d1.test1();
+        d1.test2();
+        d1.test3();
+    }
+}
