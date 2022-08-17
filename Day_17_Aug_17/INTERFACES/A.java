44d11578b87211c781c04beac4175f1c8553aa67
Suraj <1507surajkuamr@gmail.com>
2022-08-17 12:34:02 +0530

    commits

---
+++ Day_17_Aug_17/INTERFACES/A.java
@@ -0,0 +1,9 @@
+package Day_17_Aug_17.INTERFACES;
+
+public interface A {
+    //we can not create complete method in side interface
+//    public void test(){
+//
+//    } only incomplete methods are allowed
+    public void test();
+}
---
+++ Day_17_Aug_17/INTERFACES/B.java
@@ -0,0 +1,7 @@
+package Day_17_Aug_17.INTERFACES;
+//interface to interface inheritance is done using extends keyword
+public interface B extends A {
+    //test()
+    public void test1();
+
+}
