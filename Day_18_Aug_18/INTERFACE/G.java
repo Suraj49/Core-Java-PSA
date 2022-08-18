079c4e5474f8d55dd6929aca5a19b49fac54f0ff
Suraj <1507surajkuamr@gmail.com>
2022-08-19 00:43:47 +0530

    commits

--- Day_18_Aug_18/INTERFACE/G.java
+++ Day_18_Aug_18/INTERFACE/G.java
@@ -4,5 +4,8 @@ public class G implements F{
     public static void main(String[] args) {
         System.out.println(G.id);
         System.out.println(G.id2);//accessing static variable
+        //F f=new F(); we can not create object of interface
+        //but we can create ref variable of interface
+        F f=null;
     }
 }
