c0f372dc66cc4d8c7d057765157b9f01acdf7d86
Suraj <1507surajkuamr@gmail.com>
2022-08-18 20:03:49 +0530

    commits

---
+++ Day_18_Aug_18/FINAL/A.java
@@ -0,0 +1,10 @@
+package Day_18_Aug_18.FINAL;
+//final variable:- we cannot change the value of the variable if variable is final
+
+public class A {
+    public static void main(String[] args) {
+        final int i = 10;
+       // i = 20; once variable is assigned we cannot assigne new value
+        System.out.println(i);
+    }
+}
