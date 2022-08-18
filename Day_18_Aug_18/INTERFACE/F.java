7c746349905851fe712ba8779a8a7be62e95a243
Suraj <1507surajkuamr@gmail.com>
2022-08-19 00:42:06 +0530

    commits

---
+++ Day_18_Aug_18/INTERFACE/F.java
@@ -0,0 +1,9 @@
+package Day_18_Aug_18.INTERFACE;
+
+public interface F {
+    int id=20;
+    final static int id2=110; //both variable have same nature
+    //if we make variable inside interface it is final and static by default
+    // int i; error because it is by default final and we know that initialization of final variables'  req.
+}
+
---
+++ Day_18_Aug_18/INTERFACE/G.java
@@ -0,0 +1,8 @@
+package Day_18_Aug_18.INTERFACE;
+
+public class G implements F{
+    public static void main(String[] args) {
+        System.out.println(G.id);
+        System.out.println(G.id2);//accessing static variable
+    }
+}
