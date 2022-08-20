e1c4724d85521521a2b09d8f2be0310773edf9b3
Suraj <1507surajkuamr@gmail.com>
2022-08-20 22:44:31 +0530

    commits

---
+++ Day_19_Aug_19/JAVA8/LE/A.java
@@ -0,0 +1,6 @@
+package Day_19_Aug_19.JAVA8.LE;
+
+@FunctionalInterface
+public interface A {
+    public void test1();
+}
---
+++ Day_19_Aug_19/JAVA8/LE/B.java
@@ -0,0 +1,10 @@
+package Day_19_Aug_19.JAVA8.LE;
+
+public class B {
+    public static void main(String[] args) {
+        A a1 = () ->{
+            System.out.println(1);
+        };
+        a1.test1();
+    }
+}
