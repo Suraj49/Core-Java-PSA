1961c394ec1e121699f22b194cf6740e4594b2d5
Suraj <1507surajkuamr@gmail.com>
2022-08-23 18:45:42 +0530

    commits

---
+++ Day_21_Aug_23/A.java
@@ -0,0 +1,11 @@
+package Day_21_Aug_23;
+
+public class A {
+    public static void main(String[] args) {
+        int i=10;
+        int j=0;
+        int result=i/j; //exception
+        System.out.println(result);
+        System.out.println("hello");
+    }
+}
