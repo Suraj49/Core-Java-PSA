aafad9163d9eda68f253cbd1b31dbb5ba96b403c
Suraj <1507surajkuamr@gmail.com>
2022-08-25 23:18:29 +0530

    commits

---
+++ Day_23_Aug_25/D.java
@@ -0,0 +1,12 @@
+package Day_23_Aug_25;
+
+//for loop:- when we want to do any work again and again then we use concepts of loop
+
+public class D {
+    public static void main(String[] args) {
+        for (int i=0;i<10;i++){ //0 to 9 total no of 10 times loop will execute
+            System.out.println(i);
+            System.out.println("Suraj");
+        }
+    }
+}
---
+++ Day_23_Aug_25/E.java
@@ -0,0 +1,15 @@
+package Day_23_Aug_25;
+
+//if condition : -
+public class E {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5 ; i++){
+            if(i == 3){
+                System.out.println("End");
+                break;
+            }
+            System.out.println(i);
+            System.out.println("Suraj");
+        }
+    }
+}
