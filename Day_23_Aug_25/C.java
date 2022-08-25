181540feedd0c73e069871c078714da8be2a77dd
Suraj <1507surajkuamr@gmail.com>
2022-08-25 20:43:24 +0530

    commits

--- Day_23_Aug_25/C.java
+++ Day_23_Aug_25/C.java
@@ -1,4 +1,16 @@
 package Day_23_Aug_25;
-
+//Array :- in array we store collection of value
+//when we create an array a spacial object will we create
+//in array the value gets stored into index zero.
 public class C {
+    public static void main(String[] args) {
+        int [] arr=new int[3];
+        System.out.println(arr);
+        arr[0]=10;
+        arr[1]=20;
+        arr[2]=30;
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+    }
 }
