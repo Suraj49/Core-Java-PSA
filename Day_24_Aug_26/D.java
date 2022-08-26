fc5b84d29a794fa08e3718bf0aafa900e5d09030
Suraj <1507surajkuamr@gmail.com>
2022-08-26 20:34:42 +0530

    commits

---
+++ Day_24_Aug_26/D.java
@@ -0,0 +1,10 @@
+package Day_24_Aug_26;
+
+//infinite loops
+public class D {
+    public static void main(String[] args) {
+        for ( ; ; ){
+            System.out.println("Suraj");
+        }
+    }
+}
