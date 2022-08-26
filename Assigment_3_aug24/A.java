f4b2fcffc086c6b63595ecff2c90301dccf5dac9
Suraj <1507surajkuamr@gmail.com>
2022-08-26 20:17:32 +0530

    commits

---
+++ Assigment_3_aug24/A.java
@@ -0,0 +1,4 @@
+package Assigment_3_aug24;
+//Type cast:
+public class A {
+}
---
+++ Day_24_Aug_26/A.java
@@ -0,0 +1,12 @@
+package Day_24_Aug_26;
+
+public class A {
+    public static void main(String[] args) {
+        for (int i=0;i<5;i++){
+            if(i==2){
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+}
