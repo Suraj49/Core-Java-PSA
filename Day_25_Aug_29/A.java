14a1ff4df026285bb2b16648728edb59bb0884d1
Suraj <1507surajkuamr@gmail.com>
2022-08-29 17:23:34 +0530

    commits

---
+++ Day_25_Aug_29/A.java
@@ -0,0 +1,11 @@
+package Day_25_Aug_29;
+//while loop
+public class A {
+    public static void main(String[] args) {
+        int x=0;
+        while (x<3){//0,1,2
+            System.out.println(x);//0,1,2
+            x++;//1,2,3
+        }
+    }
+}
