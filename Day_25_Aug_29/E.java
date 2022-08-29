adf6817abdad43d2d69f17ebedf5427b971569ae
Suraj <1507surajkuamr@gmail.com>
2022-08-29 17:59:56 +0530

    commits

---
+++ Day_25_Aug_29/E.java
@@ -0,0 +1,13 @@
+package Day_25_Aug_29;
+
+//if else
+public class E {
+    public static void main(String[] args) {
+        int x=5;
+        if(x>5){
+            System.out.println("hello");
+        }else {
+            System.out.println("bye");
+        }
+    }
+}
---
+++ Day_25_Aug_29/F.java
@@ -0,0 +1,16 @@
+package Day_25_Aug_29;
+//if else if:
+public class F {
+    public static void main(String[] args) {
+        int x=10;
+        if (x==10){
+            System.out.println(1);
+        }else if (x==20){
+            System.out.println(2);
+        }else if(x==20){
+            System.out.println(3);
+        }else {
+            System.out.println("invalid input");
+        }
+    }
+}
