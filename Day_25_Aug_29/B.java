cdde0aae87e9f201bcd609142e7ba6b5e7ae6ca7
Suraj <1507surajkuamr@gmail.com>
2022-08-29 17:29:48 +0530

    commits

---
+++ Day_25_Aug_29/B.java
@@ -0,0 +1,11 @@
+package Day_25_Aug_29;
+//do while loop
+public class B {
+    public static void main(String[] args) {
+        int i=0;
+        do { //if condition is true or false do block run then after check the condition
+            System.out.println(i);//0,1,2
+            i++;//0,1,2
+        }while (i<3);
+    }
+}
