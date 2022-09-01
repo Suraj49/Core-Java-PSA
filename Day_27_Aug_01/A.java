0aa553017a35f13a20801fd5561bbeedb49c9692
Suraj <1507surajkuamr@gmail.com>
2022-09-01 22:07:43 +0530

    commits

--- Day_27_Aug_01/A.java
+++ Day_27_Aug_01/A.java
@@ -2,7 +2,7 @@ package Day_27_Aug_01;
 //remove all duplicate element from array and store into new array
 public class A {
     public static void main(String[] args) {
-        int []arr={1,2,2,3,4,4,5,6};
+        int []arr={1,2,2,3,4,4,5,6,7,8,8};
         int len=arr.length;
         int temp[]=new int[len];
         int j=0;
@@ -11,9 +11,9 @@ public class A {
                temp[j]=arr[i];
                j++;
             }
-
         }
-        for (int k=0;k<len;k++){
+        temp[j]=arr[len-1];
+        for (int k=0;k<j+1;k++){
             System.out.println(temp[k]);
         }
     }
