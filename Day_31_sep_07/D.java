d366e586bce7758324c448210df6cfd27c6ad4c5
Suraj <1507surajkuamr@gmail.com>
2022-09-07 23:44:17 +0530

    commits

---
+++ Day_31_sep_07/D.java
@@ -0,0 +1,23 @@
+package Day_31_sep_07;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class D {
+    public static void main(String[] args) {
+        try {
+            File f=new File("E://test_psa//test.txt");
+            long len=f.length();
+            FileReader fr =new FileReader(f);
+            char[] ch=new char[(int)len];
+            fr.read(ch);
+            for (char x:ch){
+                System.out.print(x);
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
