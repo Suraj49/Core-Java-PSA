756a9d7614f991f4d81c5e428b0bd1e21d20f880
Suraj <1507surajkuamr@gmail.com>
2022-09-13 20:37:37 +0530

    commits

---
+++ Day_36_sep_13/A.java
@@ -0,0 +1,13 @@
+package Day_36_sep_13;
+
+public class A {
+    private int id;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
---
+++ Day_36_sep_13/B.java
@@ -0,0 +1,10 @@
+package Day_36_sep_13;
+
+public class B {
+    public static void main(String[] args) {
+        A a1=new A();
+        a1.setId(40);
+        int x = a1.getId();
+        System.out.println(x);
+    }
+}
