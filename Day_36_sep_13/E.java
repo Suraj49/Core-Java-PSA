57e98d19d88994ffc6839b35f56a556192c3990c
Suraj <1507surajkuamr@gmail.com>
2022-09-13 20:44:48 +0530

    commits

---
+++ Day_36_sep_13/E.java
@@ -0,0 +1,18 @@
+package Day_36_sep_13;
+
+public class E {
+    private int id;
+    private String name;
+    public E(int id,String name){
+        this.id=id;
+        this.name=name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
---
+++ Day_36_sep_13/F.java
@@ -0,0 +1,9 @@
+package Day_36_sep_13;
+
+public class F {
+    public static void main(String[] args) {
+        E e1=new E(25,"Suraj");
+        System.out.println(e1.getId());
+        System.out.println(e1.getName());
+    }
+}
