2a7e02c1f0f9aae509f4dd4e204af5d23ce0a3f2
Suraj <1507surajkuamr@gmail.com>
2022-09-13 20:41:21 +0530

    commits

---
+++ Day_36_sep_13/D.java
@@ -0,0 +1,11 @@
+package Day_36_sep_13;
+
+public class D {
+    public static void main(String[] args) {
+        C c1=new C();
+        c1.setId(20);
+        c1.setName("Suraj");
+        System.out.println(c1.getId());
+        System.out.println(c1.getName());
+    }
+}
