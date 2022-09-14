45a5dc24f333f320f760d043c33bbb64d8d315af
Suraj <1507surajkuamr@gmail.com>
2022-09-14 20:22:53 +0530

    commits

---
+++ Day_37_sep_14/C.java
@@ -0,0 +1,12 @@
+package Day_37_sep_14;
+
+public class C {
+    public static void main(String[] args) {
+        try{
+            int i=10/3;
+            System.exit(0);
+        }finally {
+            System.out.println(100);
+        }
+    }
+}
