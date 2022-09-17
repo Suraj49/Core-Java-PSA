30a0b9c60e52c0f23bb59e60db0e12b524284520
Suraj <1507surajkuamr@gmail.com>
2022-09-17 12:45:51 +0530

    commits

--- StringAssgnment/interviewQ/A.java
+++ StringAssgnment/interviewQ/A.java
@@ -1,4 +1,10 @@
 package StringAssgnment.interviewQ;
-
+//reverse a given string
 public class A {
+    public static void main(String[] args) {
+        String str="Suraj Kumar";
+        for (int i=str.length()-1;i>=0;i--){
+            System.out.print(str.charAt(i));
+        }
+    }
 }
