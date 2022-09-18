e54de61dd76694bb12e9cf807d33105daccee1c5
Suraj <1507surajkuamr@gmail.com>
2022-09-19 00:00:09 +0530

    commits

---
+++ JDBC/Insert/C.java
@@ -0,0 +1,23 @@
+package JDBC.Insert;
+
+
+import java.sql.*;
+
+public class C {
+    public static void main(String[] args) {
+        try {
+
+            Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/test_x","root","surajtest");
+
+            Statement stm=con.createStatement();
+
+            stm.executeUpdate("DELETE FROM registration WHERE name='ramcharn'");
+
+            System.out.println(con);
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
+
