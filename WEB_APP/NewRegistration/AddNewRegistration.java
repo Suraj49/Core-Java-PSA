da95cd2cd5a943bb361e829e2621cbe349cdcf7f
Suraj <1507surajkuamr@gmail.com>
2022-09-20 22:07:19 +0530

    commits

--- WEB_APP/NewRegistration/AddNewRegistration.java
+++ WEB_APP/NewRegistration/AddNewRegistration.java
@@ -12,7 +12,7 @@
 //import javax.servlet.http.HttpServlet;
 //import javax.servlet.http.HttpServletRequest;
 //import javax.servlet.http.HttpServletResponse;
-//
+// Remove aLL comment
 //@WebServlet("/save")
 //public class AddNewRegistration extends HttpServlet {
 //    private static final long serialVersionUID = 1L;
