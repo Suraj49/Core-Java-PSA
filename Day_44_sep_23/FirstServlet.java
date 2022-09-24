9d1a5ef2785fe5b42c4a353ecdf76390205aefbc
Suraj <1507surajkuamr@gmail.com>
2022-09-25 00:10:45 +0530

    commits

--- Day_44_sep_23/FirstServlet.java
+++ Day_44_sep_23/FirstServlet.java
@@ -1,35 +1,35 @@
-package Day_44_sep_23;
-
-
-import java.io.IOException;
-
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-@WebServlet("/firstServlet")
-public class FirstServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
-
-    public FirstServlet() {
-        super();
-    }
-
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-    }
-
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-
-        String name = request.getParameter("name");
-
-        RequestDispatcher rd = request.getRequestDispatcher("secondServlet");
-        rd.forward(request, response);
-
-    }
-
-}
+//package Day_44_sep_23;
+//
+//
+//import java.io.IOException;
+//
+//import javax.servlet.RequestDispatcher;
+//import javax.servlet.ServletException;
+//import javax.servlet.annotation.WebServlet;
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//
+//@WebServlet("/firstServlet")
+//public class FirstServlet extends HttpServlet {
+//    private static final long serialVersionUID = 1L;
+//
+//    public FirstServlet() {
+//        super();
+//    }
+//
+//    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+//            throws ServletException, IOException {
+//    }
+//
+//    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+//            throws ServletException, IOException {
+//
+//        String name = request.getParameter("name");
+//
+//        RequestDispatcher rd = request.getRequestDispatcher("secondServlet");
+//        rd.forward(request, response);
+//
+//    }
+//
+//}
---
+++ Day_44_sep_23/SecondServlet.java
@@ -0,0 +1,29 @@
+package Day_44_sep_23;
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/secondServlet")
+public class SecondServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    public SecondServlet() {
+        super();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        System.out.println("do post");
+    }
+
+}
+
