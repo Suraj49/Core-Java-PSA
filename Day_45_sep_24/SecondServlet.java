9c69ce83f6aa27f42a61650417255fd098d6b639
Suraj <1507surajkuamr@gmail.com>
2022-09-26 23:50:33 +0530

    commits

---
+++ Day_45_sep_24/SecondServlet.java
@@ -0,0 +1,30 @@
+//package Day_45_sep_24;
+//
+//package p1;
+//
+//import java.io.IOException;
+//import javax.servlet.ServletException;
+//import javax.servlet.annotation.WebServlet;
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//
+//@WebServlet("/secondServlet")
+//public class SecondServlet extends HttpServlet {
+//    private static final long serialVersionUID = 1L;
+//
+//    public SecondServlet() {
+//        super();
+//    }
+//
+//    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+//            throws ServletException, IOException {
+//    }
+//
+//    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+//            throws ServletException, IOException {
+//        System.out.println("do Post");
+//        Object name = request.getAttribute("name");
+//        System.out.println((String)name);
+//    }
+//}
