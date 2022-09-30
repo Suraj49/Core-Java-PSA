ade6d56e302f87c6f8bdc9d153142c0f81ba3889
Suraj <1507surajkuamr@gmail.com>
2022-09-30 23:15:33 +0530

    commits

--- Day_47_sep_28/A.java
+++ Day_47_sep_28/A.java
@@ -1,9 +1,11 @@
 package Day_47_sep_28;
 
 import java.util.Date;
+import java.util.Scanner;
 
 public class A {
     public static void main(String[] args) {
+        Scanner scn=new Scanner(System.in);
         System.out.println(new Date());
     }
 }
