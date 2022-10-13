7a5b35f7a8a2664e62cb0cd6deffa8b7cccd61c9
Suraj <1507surajkuamr@gmail.com>
2022-10-13 18:56:15 +0530

    commits

---
+++ StringAssgnment/interviewQ/I.java
@@ -0,0 +1,10 @@
+package StringAssgnment.interviewQ;
+
+public class I {
+    public static void main(String[] args) {
+        String s1="xyz";
+        String s2="xyz";
+        String s3="abc";
+        String s4="abc";
+    }
+}
