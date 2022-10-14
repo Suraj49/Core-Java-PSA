c9f6540c92bc7a056f79629cf76eb0aace303146
Suraj <1507surajkuamr@gmail.com>
2022-10-14 19:25:47 +0530

    commits

---
+++ StringAssgnment/interviewQ/L.java
@@ -0,0 +1,11 @@
+package StringAssgnment.interviewQ;
+
+public class L {
+    public static void main(String[] args) {
+        String str="Suraj Kumar";
+        System.out.println(str.startsWith("S"));
+        System.out.println(str.startsWith("k"));
+        System.out.println(str.endsWith("r"));
+        System.out.println(str.endsWith("R"));
+    }
+}
