84cba2913d4496c1276740f141fe964b771d2221
Suraj <1507surajkuamr@gmail.com>
2022-10-14 19:32:41 +0530

    commits

---
+++ StringAssgnment/interviewQ/M.java
@@ -0,0 +1,8 @@
+package StringAssgnment.interviewQ;
+
+public class M {
+    public static void main(String[] args) {
+        String str="Suraj Kumar";
+        System.out.println(str.length());//it count white space also
+    }
+}
