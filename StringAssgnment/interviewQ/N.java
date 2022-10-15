724e72c2770a41a3c253a4b0541cceed189f5f68
Suraj <1507surajkuamr@gmail.com>
2022-10-15 19:48:19 +0530

    commits

---
+++ StringAssgnment/interviewQ/N.java
@@ -0,0 +1,19 @@
+package StringAssgnment.interviewQ;
+
+public class N {
+    public static void main(String[] args) {
+        int i=10;
+        float f=320.2f;
+        double d=235.5d;
+        boolean b=true;
+        char []c={'a','b','c'};
+        String intStr=String.valueOf(i);
+        String floatStr=String.valueOf(f);
+        String doobleStr=String.valueOf(d);
+        String boolStr=String.valueOf(b);
+        System.out.println(intStr);
+        System.out.println(floatStr);
+        System.out.println(doobleStr);
+        System.out.println(boolStr);
+    }
+}
