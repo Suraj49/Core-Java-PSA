9741024bfceb03d73a74426703483f65dd38c0cc
Suraj <1507surajkuamr@gmail.com>
2022-10-17 23:34:03 +0530

    commits

---
+++ Day_59_oct_18/A.java
@@ -0,0 +1,41 @@
+//package Day_59_oct_18;
+//
+//import org.junit.After;
+//import org.junit.AfterClass;
+//import org.junit.Before;
+//import org.junit.BeforeClass;
+//import org.junit.Test;
+//
+//public class JUNIT {
+//
+//    @Before
+//    public void BeforeTest() {
+//        System.out.println("Before Test");
+//    }
+//
+//    @Test
+//    public void teat1() {
+//        System.out.println("test1");
+//    }
+//
+//    @Test
+//    public void teat2() {
+//        System.out.println("test2");
+//    }
+//
+//    @After
+//    public void afterTest() {
+//        System.out.println("After Test");
+//    }
+//
+//    @AfterClass
+//    public static void afterClass() {
+//        System.out.println("afterClass");
+//    }
+//
+//    @BeforeClass
+//    public static void beforeClass() {
+//        System.out.println("Before Class");
+//    }
+//}
+//
