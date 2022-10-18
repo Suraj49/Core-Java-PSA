8eed72c8948e5217619a97faeed2bdbd29dd8d0b
Suraj <1507surajkuamr@gmail.com>
2022-10-18 23:48:42 +0530

    commits

---
+++ Day_60_oct_19/B.java
@@ -0,0 +1,17 @@
+package Day_60_oct_19;
+
+//import org.junit.Assert;
+//import org.junit.Test;
+//
+//public class B {
+//
+//    @Test
+//    public void testAdd() {
+//        AddNo a=new AddNo();
+//        int actualValue=a.addNo(10, 20);
+//        int exectedValue=40;
+//        Assert.assertEquals(exectedValue, actualValue);
+//
+//    }
+//}
+
